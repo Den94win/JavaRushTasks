c4d0a6d7c6c4a467534fe82bc989116f08007303
den94win <den94win@mail.ru>
2024-01-18 20:41:05 +0300

    0.0.12 Add solution JavaSyntax task0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        //напишите тут ваш код
+      int number = 0;
+      while (number < 100){
+            System.out.println(quote);
+            number ++;
+      }
+    }
+}
