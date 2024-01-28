efa2f0d8fccc9911637936d1fa6de607c4868d82
den94win <den94win@mail.ru>
2024-01-28 20:58:18 +0300

    0.0.13 Add solution JavaSyntax task0410

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0402/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0402/Solution.java
@@ -10,6 +10,12 @@ public class Solution {
     public static void main(String[] args) {
         String text = " любит меня.";
         //напишите тут ваш код
-
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int i = 0;
+        while ( i < 10){
+            System.out.println(name + text);
+            i ++;
+        }
     }
 }
