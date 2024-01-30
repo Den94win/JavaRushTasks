fe7659fc409a4b2c6c21768a5f29614579bee3aa
den94win <den94win@mail.ru>
2024-01-30 22:14:46 +0300

    0.0.15 Add solution JavaSyntax task0309

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0309/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0309/Solution.java
@@ -11,11 +11,13 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         int numberA = scanner.nextInt();
         int numberB = scanner.nextInt();
-        if (numberA > numberB) {
-            System.out.println(numberA);
-        } else {
-            System.out.println(numberB);
-        }
-
+        int numberC;
+//        if (numberA > numberB) {
+//            System.out.println(numberA);
+//        } else {
+//            System.out.println(numberB);
+//        }
+        numberC = numberA > numberB ? numberA : numberB;
+        System.out.println(numberC);
     }
 }
